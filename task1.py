1022c8cbb59193a72b0bdbc7f1cb54a5f7fecb8f
Oleksandr <merlin.ua.1@gmail.com>
2023-05-21 23:34:17 +0300

    Add ear classifier to task1 (not working)

--- task1.py
+++ task1.py
@@ -1,8 +1,9 @@
 import os
-
 import cv2
 
-raw_image = image = cv2.imread("friends.jpg")
+print('Task 1. OpenCV version: ' + cv2.__version__)
+
+raw_image = image = cv2.imread('friends.jpg')
 
 # Зменшення зображення зі збереженням відношення сторін
 h, w = image.shape[0:2]  # висота і ширина оригінального зображення
@@ -11,11 +12,13 @@ ratio = w / h  # відношення ширини до висоти
 w_new = int(h_new * ratio)  # ширина нового зображення
 image = cv2.resize(image, (w_new, h_new))  # зміна розміру оригінального зображення
 
-# Виявлення обличчя, усмішки, очей людей
+# Виявлення обличчя, усмішки, очей, людей
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
 nose_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_mcs_nose.xml')
+# left_ear_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_mcs_leftear.xml')
+# right_ear_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_mcs_rightear.xml')
 
 gray_filter = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray_filter, 1.07, 3)
@@ -36,10 +39,15 @@ for (x, y, w, h) in faces:
     for (nx, ny, nw, nh) in nose:
         cv2.rectangle(roi_color, (nx, ny), (nx + nw, ny + nh), (0, 255, 255), 1)
 
+    # left_ear = left_ear_cascade.detectMultiScale(roi_gray, 1.15)
+    # right_ear = left_ear_cascade.detectMultiScale(roi_gray, 1.15)
+    # for (nx, ny, nw, nh) in left_ear + right_ear:
+    #     cv2.rectangle(roi_color, (nx, ny), (nx + nw, ny + nh), (0, 255, 255), 1)
+
 os.makedirs('output', exist_ok=True)
 cv2.imwrite('output/task1-output.jpg', image)
 
-cv2.imshow("Task1", image)
+cv2.imshow('Task1', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
